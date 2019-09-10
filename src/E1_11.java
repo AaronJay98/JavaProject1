82526b45a1c4ce2e4657f725db61c98f72603686
Aaron Jay Macaldo <Aaronjay.macaldo@sjsu.edu>
2019-09-09 20:49:27 -0700

    First commit: Project Finished with bunny output

---
+++ src/E1_11.java
@@ -0,0 +1,15 @@
+public class E1_11 {
+
+    public static void main(String[] args) {
+        System.out.println("      /\\ /\\                           \n" +  //This is the one system.out that will output
+                           "      \\ V /       ----------------     \n" +  //to screen and it comprises of multiple
+                           "      /. .\\     /   Welcome to my  \\  \n" +  //strings added to each other to create the
+                           "     ( =v= )___/    first project!  \\  \n" +  //image of a bunny saying a greeting when
+                           "      )   (    \\    I am a bunny    /  \n" +  //run. The strings are separated by each line
+                           "     /     \\    \\   named Bonnie!  /  \n" +  //when outputted in order to be much easier
+                           "    (  ) (  )     ----------------      \n" +  //to read and much easier to make edits
+                           "    /WW   WW\\                          \n" +  //when creating the bunny
+                           "   _\\   _   /_                         \n" +
+                           "  (____)*(____)                         \n");  //This is where the system.out ends
+    }
+}
